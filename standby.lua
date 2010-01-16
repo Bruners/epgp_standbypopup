538b444699f44b5c35c21ddd57d62cbdc3d4bc1f
Bruners <bruners@gmail.com>
2010-01-16 01:03:42 +0100

    Add editbox to let people use the popup when on alternative characters

--- standby.lua
+++ standby.lua
@@ -1,40 +1,60 @@
+epgpstandbyDB = {}
 local EPGP_TRIGGER = "epgp standby"
 local TEXT_REMOVED_LIST = "You have been removed from the award list by"
 local TEXT_ADD_TO_LIST = "Add me again"
 local TEXT_CLOSE = "Raiding sucks"
-local leader, minute, seconds
+local EPGP_REMOVED_STRING = "%s is now removed from the award list"
+local leader, player, db
+local backdrop = {
+	edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border", edgeSize = 10
+}
 local f = CreateFrame("Frame")
-f:RegisterEvent("PLAYER_LOGIN")
+
+f:RegisterEvent("PLAYER_ENTERING_WORLD")
 f:SetScript("OnEvent", function(self, event, ...)
 	self[event](self, event, ...)
 end)
 
-function f:PLAYER_LOGIN()
+function f:PLAYER_ENTERING_WORLD()
+	player = UnitName("player")
+	local name = string.format("%s - %s", player, GetRealmName())
+
+	if not _G.epgpstandbyDB[name] then
+		_G.epgpstandbyDB[name] = { main = player }
+		db = _G.epgpstandbyDB[name]
+	else
+		db = _G.epgpstandbyDB[name]
+	end
+
 	f:RegisterEvent("CHAT_MSG_WHISPER")
 end
 
 function f:CHAT_MSG_WHISPER(event, msg, author, ...)
-	local player = UnitName("player")
-	if msg == (string.format("%s is now removed from the award list", player)) then
-		minute, seconds = GetGameTime()
+	if msg == (string.format(EPGP_REMOVED_STRING, player)) then
 		leader = author
 		f:Show()
 		f.font:SetText(TEXT_REMOVED_LIST)
 		f.bywho:SetText(leader)
-		f.time:SetText(string.format("%s:%s", minute, seconds))
+		f.time:SetText(date"%H:%M:%S")
+		f.name:SetText(db.main)
 	end
+format(date"%H%M.%S", text)
 end
 
 function f:ButtonClick()
 	f:Hide()
 	if leader then
-		SendChatMessage(EPGP_TRIGGER, "WHISPER", nil, leader)
+		if player == db.main then
+			SendChatMessage(EPGP_TRIGGER, "WHISPER", nil, leader)
+		else
+			SendChatMessage(string.format("%s %s",EPGP_TRIGGER, db.main), "WHISPER", nil, leader)
+		end
 	end
 end
 
 f:Hide()
 f:SetWidth(350)
-f:SetHeight(120)
+f:SetHeight(150)
 f:SetPoint("CENTER")
 f:EnableMouse(true)
 f:SetMovable(true)
@@ -62,21 +82,42 @@ time:SetPoint("TOP", bywho, "BOTTOM", 0, -5)
 
 f.time = time
 
-local button = CreateFrame("Button", nil, f, "UIPanelButtonTemplate")
-button:SetHeight(20)
-button:SetWidth(140)
-button:SetPoint("BOTTOMLEFT", f, "BOTTOMLEFT", 30, 20)
-button:SetText(TEXT_ADD_TO_LIST)
-button:RegisterForClicks("AnyUp")
-button:SetScript("OnClick", f.ButtonClick)
+local addme = CreateFrame("Button", nil, f, "UIPanelButtonTemplate")
+addme:SetHeight(20)
+addme:SetWidth(140)
+addme:SetPoint("BOTTOMLEFT", f, "BOTTOMLEFT", 30, 20)
+addme:SetText(TEXT_ADD_TO_LIST)
+addme:RegisterForClicks("AnyUp")
+addme:SetScript("OnClick", f.ButtonClick)
 
-f.button = button
+f.addme = addme
 
 local close = CreateFrame("Button", "CloseButton", f, "UIPanelButtonTemplate")
 close:SetHeight(20)
 close:SetWidth(140)
-close:SetPoint("LEFT", button, "RIGHT", 10, 0)
+close:SetPoint("LEFT", addme, "RIGHT", 10, 0)
 close:SetText(TEXT_CLOSE)
-close:SetScript("OnClick", function() f:Hide() end )
+close:SetScript("OnClick", function() f:Hide(); ReloadUI(); end )
 
 f.close = close
+
+local name = CreateFrame("EditBox", nil, f)
+name:SetPoint("CENTER", f, "CENTER", 0, -15)
+name:SetWidth(130)
+name:SetHeight(19)
+name:SetFontObject(GameFontHighlight)
+name:SetTextInsets(8,8,8,8)
+name:SetBackdrop(backdrop)
+name:SetBackdropColor(.1,.1,.1,.3)
+name:SetMultiLine(false)
+name:SetAutoFocus(false)
+name:SetText("")
+name:SetScript("OnEditFocusLost", function()
+	local newname = name:GetText()
+	if newname and newname ~= nil or newname ~= "" then
+		db.main = newname
+	end
+end)
+name:SetScript("OnEscapePressed", name.ClearFocus)
+
+f.name = name
