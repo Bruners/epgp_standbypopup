bb0a605597990587d692ddd72835e475b25c33a9
Bruners <bruners@gmail.com>
2010-01-16 06:55:29 +0100

    Add some helping tooltips to important elements

--- standby.lua
+++ standby.lua
@@ -1,9 +1,12 @@
 epgpstandbyDB = {}
 local EPGP_TRIGGER = "epgp standby"
-local TEXT_REMOVED_LIST = "You have been removed from the award list by"
-local TEXT_ADD_TO_LIST = "Add me again"
-local TEXT_CLOSE = "Raiding sucks"
 local EPGP_REMOVED_STRING = "%s is now removed from the award list"
+local TEXT_REMOVED_LIST = "You have been removed from the award list by"
+local BUTTON_TEXT_ADD = "Add me again"
+local BUTTON_TEXT_CLOSE = "Raiding sucks"
+local TOOLTIP_ALT_NICKNAME = "Nickname to be added to if you are on an alternative character"
+local TOOLTIP_BUTTON_CLOSE = "Close this window"
+local TOOLTIP_BUTTON_ADD = "Add yourself to the epgp standby list"
 local leader, player, db
 local backdrop = {
 	edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border", edgeSize = 10
@@ -51,6 +54,14 @@ function f:ButtonClick()
 	end
 end
 
+local function HideTooltip() GameTooltip:Hide() end 
+local function ShowTooltip(self)
+	if self.tiptext then
+		GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
+		GameTooltip:SetText(self.tiptext, nil, nil, nil, nil, true)
+	end
+end
+
 f:Hide()
 f:SetWidth(350)
 f:SetHeight(150)
@@ -85,9 +96,12 @@ local addme = CreateFrame("Button", nil, f, "UIPanelButtonTemplate")
 addme:SetHeight(20)
 addme:SetWidth(140)
 addme:SetPoint("BOTTOMLEFT", f, "BOTTOMLEFT", 30, 20)
-addme:SetText(TEXT_ADD_TO_LIST)
+addme:SetText(BUTTON_TEXT_ADD)
+addme.tiptext = TOOLTIP_BUTTON_ADD
 addme:RegisterForClicks("AnyUp")
 addme:SetScript("OnClick", f.ButtonClick)
+addme:SetScript("OnEnter", ShowTooltip)
+addme:SetScript("OnLeave", HideTooltip)
 
 f.addme = addme
 
@@ -95,9 +109,11 @@ local close = CreateFrame("Button", "CloseButton", f, "UIPanelButtonTemplate")
 close:SetHeight(20)
 close:SetWidth(140)
 close:SetPoint("LEFT", addme, "RIGHT", 10, 0)
-close:SetText(TEXT_CLOSE)
+close:SetText(BUTTON_TEXT_CLOSE)
+close.tiptext = TOOLTIP_BUTTON_CLOSE
 close:SetScript("OnClick", function() f:Hide() end )
-
+close:SetScript("OnEnter", ShowTooltip)
+close:SetScript("OnLeave", HideTooltip)
 f.close = close
 
 local name = CreateFrame("EditBox", nil, f)
@@ -117,6 +133,9 @@ name:SetScript("OnEditFocusLost", function()
 		db.main = newname
 	end
 end)
+name.tiptext = TOOLTIP_ALT_NICKNAME
 name:SetScript("OnEscapePressed", name.ClearFocus)
+name:SetScript("OnEnter", ShowTooltip)
+name:SetScript("OnLeave", HideTooltip)
 
 f.name = name
